1bab2fc692cb7c38f13c5012b6c8db675dd2b61c
Xavier Dury <kalgon@hotmail.com>
2016-09-24 18:27:47 +0200

    turned this reproducer in a junit test

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # jaxb-420 
-Reproducer for 
-https://java.net/jira/browse/JAXB-420
+Reproducer for https://java.net/jira/browse/JAXB-420
 
-
-Copy this project in C:\tmp\jaxb420, run `mvn`. If the output contains `isbn : eg.Isbn`, this means that global bindings are honored.
-Copy this project in C:\tmp\jaxb420ba (or change/add/remove some letters) until `mvn` output contains `isbn : java.lang.String` (this means that global bindings are ignored).
-
-Once you have one setting that works and an other one that does not work, launch `run.cmd` to build and run the project in debug mode.
-In your IDE, put a breakpoint on `com.sun.xml.xsom.impl.SchemaImpl.setAnnotation()` and connect on localhost:8888. 
-Notice that the working project only calls this method once while the failing projects calls it twice (one time with the correct annotation and a 2nd time with an empty one which overwrites the first one).
+Just run the JAXB420Test.
---
+++ src/test/java/eg/JAXB420Test.java
@@ -0,0 +1,61 @@
+package eg;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.sun.tools.xjc.api.Mapping;
+import com.sun.tools.xjc.api.Property;
+import com.sun.tools.xjc.api.SchemaCompiler;
+import com.sun.tools.xjc.api.XJC;
+
+/**
+ * This test will try to create the same model over and over until it fails.
+ * Each time, schemas'systemId are appended with the iteration number (and
+ * resolved via a custom EntityResolver) so that they may appear in a different
+ * order in
+ * {@linkplain com.sun.tools.xjc.reader.internalizer.DOMForest#getOneDocument()}.
+ * 
+ * @author Xavier Dury
+ * @see {@linkplain com.sun.tools.xjc.reader.internalizer.Internalizer#move()}
+ */
+public class JAXB420Test {
+
+	private InputSource inputSource(String systemId, String resourceName) {
+		InputSource result = new InputSource(JAXB420Test.class.getResourceAsStream(resourceName));
+		result.setSystemId(systemId);
+		return result;
+	}
+
+	@Test
+	public void simpleTest() {
+		for (int i = 0; i < 100; i++) {
+			SchemaCompiler sc = XJC.createSchemaCompiler();
+			String suffix = Integer.toString(i);
+			sc.setEntityResolver(new EntityResolver() {
+
+				public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+					if (systemId.endsWith("common.xsd")) {
+						return inputSource(systemId + suffix, "common.xsd");
+					}
+					if (systemId.endsWith("book.xsd")) {
+						return inputSource(systemId + suffix, "book.xsd");
+					}
+					return null;
+				}
+			});
+			sc.parseSchema(inputSource("bindings.xjb" + suffix, "bindings.xjb"));
+			sc.parseSchema(inputSource("root.xsd" + suffix, "root.xsd"));
+			for (Mapping mapping : sc.bind().getMappings()) {
+				for (Property property : mapping.getWrapperStyleDrilldown()) {
+					System.out.println(property.type().fullName());
+					Assert.assertEquals("Test failed at iteration " + suffix, "eg.Isbn", property.type().fullName());
+				}
+			}
+		}
+	}
+}
