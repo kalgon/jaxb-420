f3fbb13ed0290d7d96f213fc3921dfc242c917cf
Xavier DURY <Xavier.DURY@pdos-sdpsp.fgov.be>
2016-09-27 12:58:47 +0200

    removed extensions

--- src/test/java/eg/JAXB420Test.java
+++ src/test/java/eg/JAXB420Test.java
@@ -3,6 +3,7 @@ package eg;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.xml.XMLConstants;
 import javax.xml.namespace.QName;
 
 import org.junit.Assert;
@@ -17,11 +18,11 @@ import com.sun.tools.xjc.api.SchemaCompiler;
 import com.sun.tools.xjc.api.XJC;
 
 /**
- * This test will try to create the same model over and over until it fails.
- * Each time, schemas'systemId are prepended with some random prefix (and
- * resolved via a custom EntityResolver) so that they may appear in a different
- * order in
+ * This test will try to create the same model over and over and each time prepend
+ * the schemas'systemId with some random prefix (and resolved via a custom
+ * EntityResolver) so that they may appear in a different order in
  * {@linkplain com.sun.tools.xjc.reader.internalizer.DOMForest#getOneDocument()}.
+ * For some models, global bindings will be lost.
  * 
  * @author Xavier Dury
  * @see {@linkplain com.sun.tools.xjc.reader.internalizer.Internalizer#move()}
@@ -29,10 +30,11 @@ import com.sun.tools.xjc.api.XJC;
 @RunWith(Parameterized.class)
 public class JAXB420Test {
 
-	private static final QName QNAME = new QName("http://www.w3.org/2001/XMLSchema", "date");
-	
+	private static final QName DATE = new QName(XMLConstants.W3C_XML_SCHEMA_NS_URI, "date");
+
 	@Parameters(name = "prefix: ''{0}''")
 	public static List<String> prefixes() {
+
 		return Arrays.asList("", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "a", "aa", "ab", "ba", "az", "za1", "45ab", "bz4z", "46546131", "kjhkjh");
 	}
 
@@ -53,12 +55,12 @@ public class JAXB420Test {
 		});
 		sc.parseSchema(inputSource("bindings.xjb"));
 		sc.parseSchema(inputSource("root.xsd"));
-		Assert.assertEquals("java.time.LocalDate", sc.bind().getJavaType(QNAME).getTypeClass().fullName());
+		Assert.assertEquals("java.util.Calendar", sc.bind().getJavaType(DATE).getTypeClass().fullName());
 	}
 
 	private InputSource inputSource(String resourceName) {
 		InputSource result = new InputSource(getClass().getResourceAsStream(resourceName));
-		result.setSystemId(this.prefix + resourceName); // prepend systemId with prefix
+		result.setSystemId(prefix + resourceName); // prepend systemId with prefix
 		return result;
 	}
 }
