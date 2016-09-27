31fd28d5c3811a24cfffe9b63616e82648cf7d9f
Xavier DURY <Xavier.DURY@pdos-sdpsp.fgov.be>
2016-09-27 10:48:38 +0200

    even more simple

--- src/test/java/eg/JAXB420Test.java
+++ src/test/java/eg/JAXB420Test.java
@@ -29,6 +29,8 @@ import com.sun.tools.xjc.api.XJC;
 @RunWith(Parameterized.class)
 public class JAXB420Test {
 
+	private static final QName QNAME = new QName("http://www.w3.org/2001/XMLSchema", "date");
+	
 	@Parameters(name = "prefix: ''{0}''")
 	public static List<String> prefixes() {
 		return Arrays.asList("", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "a", "aa", "ab", "ba", "az", "za1", "45ab", "bz4z", "46546131", "kjhkjh");
@@ -40,23 +42,23 @@ public class JAXB420Test {
 		this.prefix = prefix;
 	}
 
-	private InputSource inputSource(String resourceName) {
-		InputSource result = new InputSource(getClass().getResourceAsStream(resourceName));
-		result.setSystemId(this.prefix + resourceName); // prepend systemId with prefix
-		return result;
-	}
-
 	@Test
 	public void customJavaTypeShouldHaveBeenRegistered() {
 		SchemaCompiler sc = XJC.createSchemaCompiler();
 		sc.setEntityResolver(new EntityResolver() {
 
 			public InputSource resolveEntity(String publicId, String systemId) {
-				return inputSource("common.xsd");
+				return inputSource("included.xsd");
 			}
 		});
 		sc.parseSchema(inputSource("bindings.xjb"));
 		sc.parseSchema(inputSource("root.xsd"));
-		Assert.assertEquals("eg.MyCustomType", sc.bind().getJavaType(new QName("urn:eg", "MyCustomType")).getTypeClass().fullName());
+		Assert.assertEquals("java.time.LocalDate", sc.bind().getJavaType(QNAME).getTypeClass().fullName());
+	}
+
+	private InputSource inputSource(String resourceName) {
+		InputSource result = new InputSource(getClass().getResourceAsStream(resourceName));
+		result.setSystemId(this.prefix + resourceName); // prepend systemId with prefix
+		return result;
 	}
 }
